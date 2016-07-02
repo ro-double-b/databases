a4780e5e511e5254750a2da338ab40b89700e154
ro-double-b <robb.marsee@gmail.com>
2016-07-01 20:08:33 -0700

    Set up schema file and loaded to mySQL

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,27 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- new_table(column1 INT NOT NULL AUTO_INCREMENT, column2 VARCHAR(10), column3 VARCHAR(10), primary key (column1));
+CREATE TABLE rooms (
+  room_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(128),
+  PRIMARY KEY (room_id)     
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE messages (
+--   /* Describe your table here.*/     
+  message_id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(128) NOT NULL,
+  msg VARCHAR(256),
+  room_id INT,
+  PRIMARY KEY (message_id),
+  FOREIGN KEY (room_id) REFERENCES rooms (room_id)
+);
 
+-- /* Create other tables and define schemas for them here! */
 
 
 
